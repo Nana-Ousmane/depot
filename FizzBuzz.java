b63472d907a507b10de02f344c8221f6df79d961
Nana-Ousmane <ck691927@ens.uqam.ca>
2020-05-22 22:25:41 -0400

    ajouter un fichier java

---
+++ FizzBuzz.java
@@ -0,0 +1,20 @@
+public class FizzBuzz
+{
+    
+    public static void main(String[]args){
+        String s="";
+        for(int i=1; i<=100 ; i++){
+            if(i%3==0){
+                s="Fizz";
+            }else if(i%5==0){
+                s="Buzz";
+            }else if(i%15==0){
+            s="FizzBuzz";
+        }else{
+            s=i+" ";
+}
+            System.out.println(s);
+        }
+    }
+}
+
